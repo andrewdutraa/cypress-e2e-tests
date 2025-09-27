76318a8255bf29a09ef972848883272233482419
Andrew Dutra <andrewdutra99@gmail.com>
2025-09-27 17:30:38 -0300

    criado cenario de cadastro com sucesso

---
+++ cypress/e2e/features/formulario.feature
@@ -0,0 +1,8 @@
+Feature: Cadastrar para cuidar de cães
+
+  Scenario: Realizar cadastro para cuidar de cães com sucesso
+     Given que estou na página de cadastro
+       And quero ser dog walker
+      When preencho os campos obrigatórios
+       And envio o formulário
+      Then devo ver a mensagem de sucesso
---
+++ cypress/e2e/features/formulario/formulario.js
@@ -0,0 +1,31 @@
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+
+Given("que estou na página de cadastro", () => {
+  cy.visit("https://walkdog.vercel.app/");
+});
+
+Given("quero ser dog walker", () => {
+  cy.contains("Quero ser Dog Walker").click();
+});
+
+When("preencho os campos obrigatórios", () => {
+    cy.get('[name="name"]').type("Andrew Dutra Jorge");
+    cy.get('[name="email"]').type("andrew@teste.com.br");
+    cy.get('[name="cpf"]').type("12345678910");
+    cy.get('[name="cep"]').type("88802400");
+    cy.intercept('https://viacep.com.br/ws/88802400/json/').as('buscarCEP')
+    cy.get('[value="Buscar CEP"]').click();
+    cy.wait('@buscarCEP', { timeout: 90000 }).its("response.statusCode").should('eq', 200)
+    cy.get('[name="addressNumber"]').type("30");
+    cy.get('[name="addressDetails"]').type("casa branca");
+    cy.contains("Cuidar").click();
+    cy.get('input[type="file"]').attachFile('uploads/documento-RG.jpeg', { force: true });
+});
+
+When("envio o formulário", () => {
+  cy.get('[class="button-register"]').click();
+});
+
+Then("devo ver a mensagem de sucesso", () => {
+  cy.contains("Recebemos o seu cadastro e em breve retornaremos o contato.").should("be.visible");
+});
